0c9e88b21e279a2d52422064374e686ab43e6b0a
ch9fod <ch9fod@gmail.com>
2017-02-05 22:28:25 -0700

    updated and tried ED3
    tried to have all the models in one file. couldnt do it

--- ED0.py
+++ ED0.py
@@ -8,7 +8,7 @@ import os
 model = AbstractModel()
 solverexe = "gurobi"
 dirsolver = r"C:\Users\ch9fod\Documents\GitHub\ED\solvers"
-datafile = "data1.dat"
+datafile = "data0.dat"
 
 
 # In[2]:
@@ -22,11 +22,6 @@ model.G = Set()
 #parameters
 model.a = Param(model.G)
 model.b = Param(model.G)
-model.Pmin = Param(model.G)
-model.Pmax = Param(model.G)
-model.d = Param(model.G)
-model.e = Param(model.G)
-model.f = Param(model.G)
 #lone parameter
 model.D = Param()
 
@@ -40,15 +35,7 @@ model.P = Var(model.G)
 # In[5]:
 
 #constraints
-def maxp(model,i):
-    return model.P[i] <= model.Pmax[i]
-model.maxprod = Constraint(model.G, rule = maxp)
-
-def minp(model,i):
-    return model.P[i] >= model.Pmin[i]
-model.minprod = Constraint(model.G, rule = minp)
-
-def demand_r(model):
+def demand_r(model,i):
     return model.D == sum(model.P[i] for i in model.G)
 model.demand = Constraint(model.G, rule = demand_r)
 
@@ -58,9 +45,7 @@ model.demand = Constraint(model.G, rule = demand_r)
 #objective
 def cost_rule(model):
     return sum(model.a[i]*model.P[i] + 
-               0.5*model.b[i]*model.P[i]**2 + 
-               10*model.d[i] + 10*model.e[i]*model.P[i] + 
-               10*model.f[i]*model.P[i]**2 for i in model.G)
+               0.5*model.b[i]*model.P[i]**2 for i in model.G)
 #default is to minimize        
 model.OBJ = Objective(rule=cost_rule) 
 
@@ -75,33 +60,18 @@ else:
 instance = model.create_instance(datafile)
 instance.dual = Suffix(direction=Suffix.IMPORT)
 results = solver.solve(instance)
+# results = solver.solve(instance, tee=True)
+#instance.solutions.load_from(results)
+#print(results)
 
 
 # In[8]:
 
 instance.display()
+# model.solutions.load_from(results)
 
 
 # In[9]:
-
-p = [0, 0, 0 ,0]
-for i in range(3):
-    p[i+1] = value(instance.P[i+1])
-
-
-# In[10]:
-
-print ("Total Emissions (tonCO2/MWh)")
-x = y = 0
-for i in range(3):
-    y = (instance.d[i+1] + instance.e[i+1]*p[i+1] + 
-        instance.f[i+1]*p[i+1]**2)
-    x = x + y
-    print(i+1, y)
-print("Total",x)
-
-
-# In[11]:
 
 print ("Duals")
 from pyomo.core import Constraint
@@ -109,5 +79,5 @@ for c in instance.component_objects(Constraint, active=True):
     print ("   Constraint",c)
     cobject = getattr(instance, str(c))
     for index in cobject:
-        print ("      ", index, instance.dual[cobject[index]])
+        print ("      ", index, cobject, instance.dual[cobject[index]])
 
--- ED1.py
+++ ED1.py
@@ -24,6 +24,7 @@ model.a = Param(model.G)
 model.b = Param(model.G)
 model.Pmin = Param(model.G)
 model.Pmax = Param(model.G)
+#added just to calculate emissions
 model.d = Param(model.G)
 model.e = Param(model.G)
 model.f = Param(model.G)
@@ -48,7 +49,7 @@ def minp(model,i):
     return model.P[i] >= model.Pmin[i]
 model.minprod = Constraint(model.G, rule = minp)
 
-def demand_r(model):
+def demand_r(model,i):
     return model.D == sum(model.P[i] for i in model.G)
 model.demand = Constraint(model.G, rule = demand_r)
 
@@ -58,9 +59,7 @@ model.demand = Constraint(model.G, rule = demand_r)
 #objective
 def cost_rule(model):
     return sum(model.a[i]*model.P[i] + 
-               0.5*model.b[i]*model.P[i]**2 + 
-               10*model.d[i] + 10*model.e[i]*model.P[i] + 
-               10*model.f[i]*model.P[i]**2 for i in model.G)
+               0.5*model.b[i]*model.P[i]**2 for i in model.G)
 #default is to minimize        
 model.OBJ = Objective(rule=cost_rule) 
 
@@ -75,6 +74,8 @@ else:
 instance = model.create_instance(datafile)
 instance.dual = Suffix(direction=Suffix.IMPORT)
 results = solver.solve(instance)
+#instance.solutions.load_from(results)
+#print(results)
 
 
 # In[8]:
